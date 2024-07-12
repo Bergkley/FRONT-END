cb26185a33fd67056155093e586e3c9a853d9f5b
Bergkley <bergkley@gmail.com>
2024-07-12 19:07:26 -0300

    feat: projeto Filmes

---
+++ PROJETO 6 - Projeto Filme/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+export default App;
