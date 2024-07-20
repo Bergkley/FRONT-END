0e29e990520863aacfcd5c70d43dd5b638a1358d
Bergkley <bergkley@gmail.com>
2024-07-20 20:56:46 -0300

    fix: refatoração

--- PROJETO 3-Calculadora/script.js
+++ PROJETO 3-Calculadora/script.js
@@ -1,27 +1,27 @@
 var box = document.getElementById("calc-display");
 
-function toScreen(x){
-  box.value+=x;
-  if(x==='C'){
-    box.value='';
+function toScreen(x) {
+  box.value += x;
+  if (x === "C") {
+    box.value = "";
   }
 }
 
-function answer(){
-  x=box.value;
-  x=eval(x);
-  box.value=x;
+function answer() {
+  x = box.value;
+  x = eval(x);
+  box.value = x;
 }
 
-function sqr(){
-  x=box.value;
-  x=eval(x*x);
-  box.value=x;
+function sqr() {
+  x = box.value;
+  x = eval(x * x);
+  box.value = x;
 }
 
-function backSpace(){
+function backSpace() {
   var num = box.value;
-  var len = num.length-1;
-  var newNum = num.substring(0,len);
+  var len = num.length - 1;
+  var newNum = num.substring(0, len);
   box.value = newNum;
 }
