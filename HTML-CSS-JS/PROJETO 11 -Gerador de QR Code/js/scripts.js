8389995cb01129077c2f527468c28a81521f87e5
Bergkley <bergkley@gmail.com>
2024-09-24 21:10:07 -0300

    Novos Projetos

---
+++ HTML-CSS-JS/PROJETO 11 -Gerador de QR Code/js/scripts.js
@@ -0,0 +1,42 @@
+// Seleção	de elementos
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Função
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerHTML = "Gerando código";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load",()=>{
+        container.classList.add("active")
+        qrCodeBtn.innerHTML = "Código criado";
+    })
+}
+
+// Eventos
+qrCodeBtn.addEventListener("click", ()=>{
+    generateQrCode();
+});
+
+qrCodeBtn.addEventListener("keydown", (e)=>{
+    if(e.code === "Enter"){
+        generateQrCode();
+    }
+});
+
+// Limpar
+qrCodeInput.addEventListener("keyup", ()=>{
+    if(!qrCodeInput.value){
+        container.classList.remove("active")
+        qrCodeBtn.innerHTML = "Gerar QR Code";
+    }
+})
